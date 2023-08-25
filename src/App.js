834d23c34f435f09685ebc3d7033dd4e595a69bb
DaSherman0210 <tgerman181@gmail.com>
2023-08-25 09:27:07 -0500

    feat: :heavy_plus_sign: Added dependencies
    Added all the deppendecies for the proyect

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Card from "./components/Card/card.component";
+
+function App() {
+   return(
+      <div>
+         <h1>E-COMMERCE IN REACT AND SNIPCART</h1>
+         <Card></Card>
+      </div>
+   )
+}
+
+export default App;
---
+++ src/components/Card/card.component.js
@@ -0,0 +1,17 @@
+function Card() {
+   return(
+      <div>
+         <div>
+            <img src="../../public/assets/camisaBlanca.jpg" />   
+         </div>
+         <div>
+            <h2>T-Shirt</h2>
+            <p>A WHite t-shirt</p>
+            <h2><strong>$35</strong></h2>
+         </div>
+         <button>Add to Cart</button>
+      </div>
+   )
+}
+
+export default Card;
