8ff8eef7e853803725c5ca7e0a3d48f66c9b1f3a
DaSherman0210 <tgerman181@gmail.com>
2023-08-25 19:06:55 -0500

    feat: :sparkles: Finished the exercise
    The exercise was finished adding more features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import imagenCamisa from "./components/Card/assets/whiteT-shirt.jpeg";
+import imagenBilletera from "./components/Card/assets/wallet.jpg";
+import imagenTaza from "./components/Card/assets/cup.jpg";
+
 import Card from "./components/Card/card.component";
 
 function App() {
+
+   const cards = [
+      {
+         img: imagenCamisa,
+         name: "T-Shirt",
+         description: "A White T-Shirt",
+         price: 35
+      },
+      {
+         img: imagenBilletera,
+         name: "Wallet",
+         description: "A fancy wallet",
+         price: 20
+      },
+      {
+         img: imagenTaza,
+         name: "Cup",
+         description: "A tea cup",
+         price: 5
+      }
+   ]
+
    return(
-      <div>
-         <h1>E-COMMERCE IN REACT AND SNIPCART</h1>
-         <Card></Card>
+      <div className="main">
+         <h1>E-Commerce in React and SnipCart</h1>
+         <Card items={cards}></Card>
       </div>
    )
 }
--- src/components/Card/card.component.js
+++ src/components/Card/card.component.js
@@ -1,15 +1,27 @@
-function Card() {
-   return(
-      <div>
-         <div>
-            <img src="../../public/assets/camisaBlanca.jpg" />   
-         </div>
-         <div>
-            <h2>T-Shirt</h2>
-            <p>A WHite t-shirt</p>
-            <h2><strong>$35</strong></h2>
-         </div>
-         <button>Add to Cart</button>
+import InfoItem from "../InfoItem/infoItem.component";
+import "./card.component.css"
+
+function Card(props) {
+   return (
+      <div className="cartas">
+         <InfoItem 
+            img={props.items[0].img}
+            name={props.items[0].name}
+            description={props.items[0].description}
+            price={props.items[0].price}
+         />
+         <InfoItem 
+            img={props.items[1].img}
+            name={props.items[1].name}
+            description={props.items[1].description}
+            price={props.items[1].price}
+         />
+         <InfoItem 
+            img={props.items[2].img}
+            name={props.items[2].name}
+            description={props.items[2].description}
+            price={props.items[2].price}
+         />
       </div>
    )
 }
